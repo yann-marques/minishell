3f0a1d631ea2b093e6d39f2c5e5763899d1a180e
Yann Olive <yanolive@2F2.42angouleme.fr>
2024-04-02 18:36:47 +0200

    edit t_env_to_strtab fonction

---
+++ t_env_to_strtab.c
@@ -0,0 +1,29 @@
+
+char	**t_env_to_strtab(t_env *env)
+{
+	char	**envp;
+	t_env	*tmp_env;
+	char	*tmp;
+	int		k;
+
+	envp = malloc(sizeof(char *) * (env_size(env) + 1));
+	if (!envp)
+		return (NULL);
+	tmp_env = env;
+	k = 0;
+	while (tmp_env)
+	{
+		tmp = ft_strjoin(tmp_env->var, "=");
+		envp[k] = ft_strjoin(tmp, tmp_env->value);
+		if (tmp)
+			free(tmp);
+		if (!envp[k])
+		{
+			strtab_clear(envp);
+			return (NULL);
+		}
+		++k;
+		tmp_env = tmp_env->next; 
+	}
+	return (envp);
+}
